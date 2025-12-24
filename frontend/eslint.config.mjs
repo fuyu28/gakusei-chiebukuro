bb8f60ae2f7197114f88cb224bed32c36d420ac8
fuyu <88916226+fuyu28@users.noreply.github.com>
2025-12-24 16:55:37 +0900

    chore: next lintからeslintに変更

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+export default defineConfig([
+    {
+        ignores: [
+            "**/.vercel/**",
+            "**/.next/**",
+            "**/next-env.d.ts",
+        ],
+    },
+    {
+        extends: compat.extends("next/core-web-vitals", "next/typescript"),
+    },
+]);
